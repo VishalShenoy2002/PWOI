c72001bba1b6c54e9e9151d992d2b7edebc3b092
VishalShenoy2002 <vishal.bshenoy@gmail.com>
2021-05-04 09:45:55 +0530

    Added a function called get_data.
    It extracts data using csv.DictReader() and gives a dictionary.
    Then it appends all the field into a list and return a list of records
    with the header.

--- pwoi.py
+++ pwoi.py
@@ -1,1 +1,18 @@
 import csv
+
+# Creating s function called get_data to extract data from the given csv file
+def get_data(csv_file):
+    '''This function extracts the data from the given csv file'''
+
+    records=[]
+
+    with open(csv_file,'r') as f:
+        data=csv.DictReader(f)
+        records.append(['Date', 'Open', 'High', 'Low', 'Close'])
+
+        for element in data:
+            records.append([element['Date'].strip(),element['Open'].strip(),element['High'].strip(),element['Low'].strip(),element['Close'].strip()])
+        
+        f.close()
+    
+    return records
