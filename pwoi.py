89360259c9b0a8fe0a842bad3c6077310ff6a2a6
VishalShenoy2002 <vishal.bshenoy@gmail.com>
2021-05-04 12:57:32 +0530

    Added write_data and changed get_data
    get_data now returns a list of dictionaries. write_data takes 2 parameters that is file_to_read and file_to_write.
    it uses get data to read the file and writes it into another file using DictWriter in the csv module.

--- pwoi.py
+++ pwoi.py
@@ -4,15 +4,44 @@ import csv
 def get_data(csv_file):
     '''This function extracts the data from the given csv file'''
 
-    # Creating a List named records so that it stores all the records
+    datestring=csv_file.split('.')[0]
+    # Creating a List named records to store all the dictionaries
     records=[]
 
     # Opening the CSV file and reading it
     with open(csv_file,'r') as f:
-        reader=csv.reader(f)
+        reader=csv.DictReader(f)
 
         for record in reader:
-            records.append(record)
-        f.close()
+            dict_record={}
+            dict_record['Date']='{}-{}-{}'.format(datestring[53:55],datestring[55:57],datestring[57:])
+            for key,value in record.items():
+                dict_record[key]=value
+            records.append(dict_record)
         
+        f.close()
     return records
+
+def write_data(file_to_read,file_to_write=''):
+    '''Writes the data into csv files'''
+
+    # Extracting the data to write
+    data=get_data(file_to_read)
+
+    # Getting the keys from the first record and storing it in field_names variable
+    field_names=[]
+
+    for i in range(1):
+        keys=data[i].keys()
+
+    for key in keys:
+        field_names.append(key)
+    
+    with open(file_to_write,'a',newline='') as f:
+        writer=csv.DictWriter(f,fieldnames=field_names)
+        writer.writeheader()
+        writer.writerows(data)
+        f.close()
+    print('File Written')
+
+
